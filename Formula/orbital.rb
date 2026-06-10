8f3a77ed755cffecc154cfcbdcce836ff0457af4
danieldn-armada <daniel.nguyen@armada.ai>
2026-06-10 07:48:36 -0700

    orbctl v0.0.2

--- Formula/orbital.rb
+++ Formula/orbital.rb
@@ -1,14 +1,14 @@
 class Orbital < Formula
   desc "Orbital CLI — authenticate and interact with the Orbital cloud service"
   homepage "https://github.com/danieldn-aramada/orbital"
-  version "0.0.3"
+  version "0.0.1"
 
   if Hardware::CPU.arm?
-    url "https://github.com/danieldn-aramada/orbital/releases/download/cli/v0.0.3/orbital_v0.0.3_darwin_arm64.tar.gz"
-    sha256 "d6cc62d928b3b3f3d1ec26f575b69bba1d722eddf274657f64ac7e128848d7b7"
+    url "https://github.com/danieldn-aramada/orbital/releases/download/cli/v0.0.1/orbital_v0.0.1_darwin_arm64.tar.gz"
+    sha256 "d43efcb0ded9cc8fa4674962385580d5b529c75e3b41cd95e73561650de53adf"
   else
-    url "https://github.com/danieldn-aramada/orbital/releases/download/cli/v0.0.3/orbital_v0.0.3_darwin_amd64.tar.gz"
-    sha256 "387bcc49fab36e89a987bd445735402301085355939de73e0973984f6f1cf0a0"
+    url "https://github.com/danieldn-aramada/orbital/releases/download/cli/v0.0.1/orbital_v0.0.1_darwin_amd64.tar.gz"
+    sha256 "a1fa3536d3c082d9409b2615f03039deab945227350861cae206104a0ae7a14b"
   end
 
   def install
