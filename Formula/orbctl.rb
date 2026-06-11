622eb42a9c9fafede2533c766e6bcbfa0858805c
danieldn-armada <daniel.nguyen@armada.ai>
2026-06-11 11:23:37 -0700

    orbctl v0.0.3

--- Formula/orbctl.rb
+++ Formula/orbctl.rb
@@ -5,10 +5,10 @@ class Orbctl < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/danieldn-aramada/orbital/releases/download/cli/v0.0.3/orbctl_v0.0.3_darwin_arm64.tar.gz"
-    sha256 "a6690d89606f0635653a9d61b9220b664dc46126259686cf6f134d0294989ced"
+    sha256 "c1945cd6cb39dedb693085255e69967aaaffd4a5f462705f4b731c8da1f0c033"
   else
     url "https://github.com/danieldn-aramada/orbital/releases/download/cli/v0.0.3/orbctl_v0.0.3_darwin_amd64.tar.gz"
-    sha256 "cfb994b347f74a5e6da41d6f9f75107582b07ac335425984699cf15464830887"
+    sha256 "fca2ab220080fc33813672cb9932abb497eb67ba3c6b6b270380462a2fb6b547"
   end
 
   def install
